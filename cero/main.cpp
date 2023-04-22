96bce87976398ba6363063c60d7e0c237bd077dc
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2023-04-22 09:55:31 -0300

    proyectos con c++ de la UTN

---
+++ cero/main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    int a = 3;
+    int b = 3;
+
+    cout<<a+b<<endl;
+
+    for (int i = 0; i < a; i++){
+        cout << "soy el numero: " + char(i) << endl;
+    }
+
+    return 0;
+}
